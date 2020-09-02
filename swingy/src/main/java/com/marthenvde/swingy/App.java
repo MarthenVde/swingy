8994f72ea015ea65cc2cfca5619b7a44348ff5cc
Marthen <marthen.vandereems@gmail.com>
2020-09-02 10:25:04 +0200

    Created maven project

---
+++ swingy/src/main/java/com/marthenvde/swingy/App.java
@@ -0,0 +1,12 @@
+package com.marthenvde.swingy;
+
+import com.marthenvde.swingy.GameEngine.GameEngine;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        GameEngine.buildWorld();
+    }
+}
---
+++ swingy/src/main/java/com/marthenvde/swingy/GameEngine/GameEngine.java
@@ -0,0 +1,7 @@
+package com.marthenvde.swingy.GameEngine;
+
+public class GameEngine {
+    public static void buildWorld() {
+        System.out.println("Buidling");
+    }
+}
